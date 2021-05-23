7f3d86e6e85e72b86ccb8ff64075d221db491406
Alban Mouton <alban.mouton@gmail.com>
2021-05-23 22:47:54 +0200

    feat: add ftp and sftp support

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const util = require('util')
+const fs = require('fs-extra')
+const pump = util.promisify(require('pump'))
+const eventToPromise = require('event-to-promise')
+const path = require('path')
+const FormData = require('form-data')
+
 function displayBytes (aSize) {
   aSize = Math.abs(parseInt(aSize, 10))
   if (aSize === 0) return '0 octets'
@@ -7,13 +14,39 @@ function displayBytes (aSize) {
   }
 }
 
-exports.run = async ({ pluginConfig, processingConfig, processingId, tmpDir, axios, log, patchConfig }) => {
-  const util = require('util')
-  const pump = util.promisify(require('pump'))
-  const fs = require('fs-extra')
-  const path = require('path')
-  const FormData = require('form-data')
+const fetchHTTP = async (processingConfig, tmpFile, axios) => {
+  const opts = { responseType: 'stream' }
+  if (processingConfig.username && processingConfig.password) {
+    opts.auth = {
+      username: process.username,
+      password: processingConfig.password
+    }
+  }
+  const res = await axios.get(processingConfig.url, opts)
+  await pump(res.data, fs.createWriteStream(tmpFile))
+  if (res.headers['content-disposition']) return res.headers['content-disposition'].match(/filename="(.*)"/)[1]
+}
 
+const fetchSFTP = async (processingConfig, tmpFile) => {
+  const url = new URL(processingConfig.url)
+  const SFTPClient = require('ssh2-sftp-client')
+  const sftp = new SFTPClient()
+  await sftp.connect({ host: url.host, port: url.port, username: processingConfig.username, password: processingConfig.password })
+  await sftp.get(url.pathname, tmpFile)
+}
+
+const fetchFTP = async (processingConfig, tmpFile) => {
+  const url = new URL(processingConfig.url)
+  const FTPClient = require('ftp')
+  const ftp = new FTPClient()
+  ftp.connect({ host: url.host, port: url.port, user: processingConfig.username, password: processingConfig.password })
+  await eventToPromise(ftp, 'ready')
+  ftp.get = util.promisify(ftp.get)
+  const stream = await ftp.get(url.pathname)
+  await pump(stream, fs.createWriteStream(tmpFile))
+}
+
+exports.run = async ({ pluginConfig, processingConfig, processingId, tmpDir, axios, log, patchConfig }) => {
   if (processingConfig.datasetMode === 'update') {
     await log.step('Vérification du jeu de données')
     const dataset = (await axios.get(`api/v1/datasets/${processingConfig.dataset.id}`)).data
@@ -22,28 +55,38 @@ exports.run = async ({ pluginConfig, processingConfig, processingId, tmpDir, axi
   }
 
   await log.step('Téléchargement du fichier')
-  const res = await axios.get(processingConfig.url, { responseType: 'stream' })
   const tmpFile = path.join(tmpDir, 'file')
   // creating empty file before streaming seems to fix some weird bugs with NFS
   await fs.ensureFile(tmpFile)
-  await pump(res.data, fs.createWriteStream(tmpFile))
+
+  const url = new URL(processingConfig.url)
+  let filename = decodeURIComponent(path.parse(processingConfig.url).base)
+  if (url.protocol === 'http:' || url.protocol === 'https:') {
+    filename = await fetchHTTP(processingConfig, tmpFile, axios) || filename
+  } else if (url.protocol === 'sftp:') {
+    await fetchSFTP(processingConfig, tmpFile)
+  } else if (url.protocol === 'ftp:' || url.protocol === 'ftps:') {
+    await fetchFTP(processingConfig, tmpFile)
+  } else {
+    throw new Error(`protocole non supporté "${url.protocol}"`)
+  }
+
   // Try to prevent weird bug with NFS by forcing syncing file before reading it
   const fd = await fs.open(tmpFile, 'r')
   await fs.fsync(fd)
   await fs.close(fd)
-  const filename = res.headers['content-disposition'] ? res.headers['content-disposition'].match(/filename="(.*)"/)[1] : decodeURIComponent(path.parse(processingConfig.url).base)
   await log.info(`le fichier a été téléchargé (${filename})`)
 
   await log.step('Chargement vers le jeu de données')
   const formData = new FormData()
-  if (processingConfig.dataset.title) formData.append('title', processingConfig.dataset.title)
+  if (processingConfig.dataset && processingConfig.dataset.title) formData.append('title', processingConfig.dataset.title)
   formData.append('file', fs.createReadStream(tmpFile), { filename })
   formData.getLength = util.promisify(formData.getLength)
   const contentLength = await formData.getLength()
   await log.info(`chargement de (${displayBytes(contentLength)})`)
   const dataset = (await axios({
     method: 'post',
-    url: processingConfig.dataset.id ? `api/v1/datasets/${processingConfig.dataset.id}` : 'api/v1/datasets',
+    url: (processingConfig.dataset && processingConfig.dataset.id) ? `api/v1/datasets/${processingConfig.dataset.id}` : 'api/v1/datasets',
     data: formData,
     maxContentLength: Infinity,
     maxBodyLength: Infinity,
--- test/test.js
+++ test/test.js
@@ -7,36 +7,47 @@ const assert = require('assert').strict
 const fs = require('fs-extra')
 const downloadFile = require('../')
 
+const axiosInstance = axios.create()
+axiosInstance.interceptors.request.use(cfg => {
+  if (!/^https?:\/\//i.test(cfg.url)) {
+    if (cfg.url.startsWith('/')) cfg.url = config.dataFairUrl + cfg.url
+    else cfg.url = config.dataFairUrl + '/' + cfg.url
+  }
+  if (cfg.url.startsWith(config.dataFairUrl)) {
+    cfg.headers['x-apiKey'] = config.dataFairAPIKey
+  }
+  return cfg
+}, error => Promise.reject(error))
+// customize axios errors for shorter stack traces when a request fails
+axiosInstance.interceptors.response.use(response => response, error => {
+  if (!error.response) return Promise.reject(error)
+  delete error.response.request
+  delete error.response.headers
+  error.response.config = { method: error.response.config.method, url: error.response.config.url, data: error.response.config.data }
+  if (error.response.config.data && error.response.config.data._writableState) delete error.response.config.data
+  if (error.response.data && error.response.data._readableState) delete error.response.data
+  return Promise.reject(error.response)
+})
+
+const log = {
+  step: (msg) => console.log(chalk.blue.bold.underline(`[${dayjs().format('LTS')}] ${msg}`)),
+  error: (msg, extra) => console.log(chalk.red.bold(`[${dayjs().format('LTS')}] ${msg}`), extra),
+  warning: (msg, extra) => console.log(chalk.red(`[${dayjs().format('LTS')}] ${msg}`), extra),
+  info: (msg, extra) => console.log(chalk.blue(`[${dayjs().format('LTS')}] ${msg}`), extra),
+  debug: (msg, extra) => {
+    // console.log(`[${dayjs().format('LTS')}] ${msg}`, extra)
+  }
+}
+
 describe('Download file processing', () => {
   it('should expose a processing config schema for users', async () => {
     const schema = require('../processing-config-schema.json')
     assert.equal(schema.type, 'object')
   })
 
-  it('should run a task', async function () {
+  it('should download a file over http', async function () {
     this.timeout(10000)
 
-    const axiosInstance = axios.create()
-    axiosInstance.interceptors.request.use(cfg => {
-      if (!/^https?:\/\//i.test(cfg.url)) {
-        if (cfg.url.startsWith('/')) cfg.url = config.dataFairUrl + cfg.url
-        else cfg.url = config.dataFairUrl + '/' + cfg.url
-      }
-      if (cfg.url.startsWith(config.dataFairUrl)) {
-        cfg.headers['x-apiKey'] = config.dataFairAPIKey
-      }
-      return cfg
-    }, error => Promise.reject(error))
-    // customize axios errors for shorter stack traces when a request fails
-    axiosInstance.interceptors.response.use(response => response, error => {
-      if (!error.response) return Promise.reject(error)
-      delete error.response.request
-      delete error.response.headers
-      error.response.config = { method: error.response.config.method, url: error.response.config.url, data: error.response.config.data }
-      if (error.response.config.data && error.response.config.data._writableState) delete error.response.config.data
-      if (error.response.data && error.response.data._readableState) delete error.response.data
-      return Promise.reject(error.response)
-    })
     const processingConfig = {
       dataset: { title: 'Download file test' },
       url: 'https://koumoul.com/s/data-fair/api/v1/datasets/confinements-mondiaux/raw'
@@ -46,15 +57,7 @@ describe('Download file processing', () => {
       pluginConfig: {},
       processingConfig,
       axios: axiosInstance,
-      log: {
-        step: (msg) => console.log(chalk.blue.bold.underline(`[${dayjs().format('LTS')}] ${msg}`)),
-        error: (msg, extra) => console.log(chalk.red.bold(`[${dayjs().format('LTS')}] ${msg}`), extra),
-        warning: (msg, extra) => console.log(chalk.red(`[${dayjs().format('LTS')}] ${msg}`), extra),
-        info: (msg, extra) => console.log(chalk.blue(`[${dayjs().format('LTS')}] ${msg}`), extra),
-        debug: (msg, extra) => {
-          // console.log(`[${dayjs().format('LTS')}] ${msg}`, extra)
-        }
-      },
+      log,
       tmpDir: 'data/tmp',
       async patchConfig (patch) {
         console.log('received config patch', patch)
@@ -65,5 +68,61 @@ describe('Download file processing', () => {
     assert.ok(processingConfig.dataset.title.startsWith('Download file test'))
     const datasetId = processingConfig.dataset.id
     assert.ok(datasetId.startsWith('download-file-test'))
+  })
+
+  it('should download a file over sftp', async function () {
+    this.timeout(10000)
+
+    const processingConfig = {
+      dataset: { title: 'Download file test sftp' },
+      // cf https://test.rebex.net/
+      url: 'sftp://test.rebex.net/pub/example/readme.txt',
+      username: 'demo',
+      password: 'password'
+    }
+    await fs.ensureDir('data/tmp')
+    await downloadFile.run({
+      pluginConfig: {},
+      processingConfig,
+      axios: axiosInstance,
+      log,
+      tmpDir: 'data/tmp',
+      async patchConfig (patch) {
+        console.log('received config patch', patch)
+        Object.assign(processingConfig, patch)
+      }
+    })
+    assert.equal(processingConfig.datasetMode, 'update')
+    assert.ok(processingConfig.dataset.title.startsWith('Download file test sftp'))
+    const datasetId = processingConfig.dataset.id
+    assert.ok(datasetId.startsWith('download-file-test-sftp'))
+  })
+
+  it('should download a file over ftp', async function () {
+    this.timeout(10000)
+
+    const processingConfig = {
+      dataset: { title: 'Download file test ftp' },
+      // cf https://test.rebex.net/
+      url: 'ftp://test.rebex.net/pub/example/readme.txt',
+      username: 'demo',
+      password: 'password'
+    }
+    await fs.ensureDir('data/tmp')
+    await downloadFile.run({
+      pluginConfig: {},
+      processingConfig,
+      axios: axiosInstance,
+      log,
+      tmpDir: 'data/tmp',
+      async patchConfig (patch) {
+        console.log('received config patch', patch)
+        Object.assign(processingConfig, patch)
+      }
+    })
+    assert.equal(processingConfig.datasetMode, 'update')
+    assert.ok(processingConfig.dataset.title.startsWith('Download file test ftp'))
+    const datasetId = processingConfig.dataset.id
+    assert.ok(datasetId.startsWith('download-file-test-ftp'))
   })
 })
